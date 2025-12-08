0119f4cbcc5e6fc548353edf2833a195c80a471e
Braden McLain <bradenm@mopine.com>
2025-12-08 01:20:25 -0600

    added poster symposium survey

--- poster_symposium_survey.py
+++ poster_symposium_survey.py
@@ -27,16 +27,40 @@ if "awaiting_confirm" not in st.session_state:
 	
 def login_screen():
 	st.header("This app is private.")
-	st.subheader("Please log in.")
+	st.subheader("Please log in with your university email.")
 	st.button("Log in with Google", on_click=st.login)
 
 def all_scores_filled(ind_scores):
 	scored_filled = True
 	for response_dict in ind_scores:
 		for key in response_dict.keys():
-			if response_dict[key] == None or response_dict[key] == '':
+			if response_dict[key] == None:
 				scored_filled = False
 	return scored_filled
+
+#submission dialog popup
+@st.dialog("Feedback submitted!")
+def show_review_prompt():
+    # fixed quoting inside f-string below
+    st.success(f"Thanks for your feedback, {st.session_state['active_user'].split(' ')[0]}!")
+    st.write("Would you like to review another group? If so, please select 'Review Again' below. Otherwise, you may now close the survey.")
+    if st.button("Review Again"):
+        st.session_state['review_more_dialog'] = "Yes"
+        st.session_state.dialog_completed = True
+        st.rerun()
+
+#if user requests to review another, launch new session. Delete old data
+if st.session_state.dialog_completed and st.session_state.get("review_more_dialog") == "Yes":
+    keys_to_clear = [key for key in st.session_state if (
+        key.startswith("ind_") or 
+        key.startswith("active_group")
+    )]
+    for key in keys_to_clear:
+        del st.session_state[key]
+
+    # Reset flag so this only happens once
+    st.session_state.dialog_completed = False
+    st.rerun()
 
 
 allowed_email_list = []
@@ -62,9 +86,9 @@ for entry in st.secrets["class_list"]["students"]:
 
 def is_allowed(sign_in_id: str) -> bool:
 	is_allowed = False
-	for email_id in student_dict.keys():
-		if email_id == sign_in_id:
-			is_allowed = True
+	domain = sign_in_id.split('@')[-1]
+	if domain == 'mst.edu' or domain == 'umsystem.edu':
+		is_allowed = True
 	return is_allowed
 
 students_by_section_group = {s: dict(groups) for s, groups in nested.items()}
@@ -74,11 +98,6 @@ students_by_section = {
 	for section, groups in students_by_section_group.items()
 }
 
-#submission dialog popup
-@st.dialog("Feedback submitted!")
-def show_review_prompt():
-	# fixed quoting inside f-string below
-	st.success(f"Thanks for your feedback {st.session_state['active_user'].split(' ')[0]}! Your responses have been recorded.")
 
 def init_firebase():
 	# Only initialize once
@@ -90,97 +109,68 @@ def init_firebase():
 
 init_firebase()
 #write response to Proposal database
-ref = db.reference("Midterm_Peer_Evaluations")
+
 
 if st.user.is_logged_in:
 	# Safely extract claims (st.user is dict-like)
 	sign_in_dict = dict(st.user)
-	sign_in_id = sign_in_dict.get("email").split('@')[0]
+	sign_in_id = sign_in_dict.get("email")
 	sign_in_name = sign_in_dict.get("name", "User")
+
+	st.session_state['active_user'] = sign_in_name
 
 	if is_allowed(sign_in_id):
 		
 		st.success(f"You are successfully logged in, {sign_in_name}.")
 		####Begin UI Here
-		st.title("ME4842 Midterm Peer Evaluation")
-		st.header("Student Identification")
+		st.title("ME4842 Poster Symposium Evaluation")
 
 		ind_scores = []
 
-		#user identification, must be sequential
-		# section_dropdown = st.selectbox("Select your section",options=["Click to Select"] + list(students_by_section.keys()),key='active_section')
-		st.session_state['active_section'] = student_dict[sign_in_id]['section']
-		st.session_state['active_group'] = student_dict[sign_in_id]['group']
-		st.session_state['active_user'] = student_dict[sign_in_id]['name']
+		ref = db.reference(f"Poster_Symposium_Evaluation/{st.session_state['active_user']}")
 
-		ref = db.reference(f"Midterm_Peer_Evaluations/{st.session_state['active_user']}")
 		
-		st.markdown(f'#### Your Name: :green[{st.session_state["active_user"]}]')
-		st.markdown(f'#### Your Group: :green[{st.session_state["active_group"]}]')
-		st.write(f'If this is incorrect please contact Braden at btmywv@mst.edu')
-		# ------- Name selection with "I am an instructor" flow + passphrase (persistent) -------
-		# if st.session_state['active_section'] != 'Click to Select':
-		# 	# include instructor sentinel option
-		# 	name_options = ["Click to Select"] + list(students_by_section[st.session_state['active_section']])
+		all_groups = sorted({g for section_groups in students_by_section_group.values() for g in section_groups.keys()})
 
-		# 	user = st.selectbox("Select your name", options=name_options, key='active_user')
+		group = st.selectbox("Select the group that is presenting",	options=["Click to Select"] + all_groups,key="active_group",index=0	)
 
 		#Select group to review
 		st.header("Peer Evaluation")
-		if st.session_state['active_section'] != 'Click to Select':
-			group = st.selectbox("Select the group that is presenting",	options=["Click to Select"] + sorted(list(students_by_section_group[st.session_state['active_section']].keys())),key='active_group',index=0)
+		if st.session_state['active_group'] != 'Click to Select':
+			st.markdown(f"##### Numerical grade of **completeness**")
+			completeness =  st.number_input(f"Did the group complete the experiment? Did they take measurements to identify sources of error? Should they have done more testing? (Enter a number between 0 and 100. 0 = poor and 100 = outstanding", key=f"completeness_{group}", min_value=0.0, max_value=100.0, step=0.1, format="%.1f", value=None)
+			st.markdown(f"##### Numerical grade of **technical content**")
+			technical_content =  st.number_input(f"Technical content – the technical difficulty of the experiment, and quality of the technical content presented. (Enter a number between 0 and 100. 0 = poor and 100 = outstanding)", key=f"content_{group}", min_value=0.0, max_value=100.0, step=0.1, format="%.1f", value=None)
+			st.markdown(f"##### Numerical grade of **presentation quality**")
+			presentation_quality =  st.number_input(f"Quality of presentation – clarity and organization of presentation in terms of layout, visual aids, and examples of hardware. (Enter a number between 0 and 100. 0 = poor and 100 = outstanding)", key=f"quality_{group}", min_value=0.0, max_value=100.0, step=0.1, format="%.1f", value=None)
+			st.markdown(f"##### Numerical grade of group's **ability to answer questions**")
+			ability_to_answer_questions =  st.number_input(f"Ability to answer questions – clarity, completeness, and precision of answers to any questions regarding the presented experiment. (Enter a number between 0 and 100. 0 = poor and 100 = outstanding)", key=f"questions_{group}", min_value=0.0, max_value=100.0, step=0.1, format="%.1f", value=None)
 
-			if group != 'Click to Select':
-				students = students_by_section_group[st.session_state['active_section']][group]
-				for student in students:
-					st.subheader(f"**Score {'Yourself' if student == st.session_state.active_user else student}:**")
-					
-					st.markdown(f"##### Evaluation of {'your' if student == st.session_state.active_user else student+"'s"} participation during the three standard lab experiments")
-					labs = st.number_input(f"How much this person contributed to the three standard lab experiments on a scale from 0 to 5. 0 is no contribution, 5 is full expected individual contribution.", key=f"labs_{student}", min_value=0.0, max_value=5.0, step=0.1, format="%.1f", value=None)
-					st.markdown(f"##### Evaluation of {'your' if student == st.session_state.active_user else student+"'s"} contribution to Memo 2 and Memo 3"); 
-					memos = st.number_input(f"How much work this person contributed to the group memo assignments on a scale from 0 to 5. 0 is no contribution, 5 is full expected individual contribution.", key=f"memos_{student}", min_value=0.0, max_value=5.0, step=0.1, format="%.1f", value=None)
-					st.markdown(f"##### Evaluation of {'your' if student == st.session_state.active_user else student+"'s"} participation during group discussions and meetings"); 
-					meetings = st.number_input(f"How much this person contributed to the group discussions and meetings on a scale from 0 to 5. 0 is no contribution, 5 is full expected individual contribution.", key=f"meetings_{student}", min_value=0.0, max_value=5.0, step=0.1, format="%.1f", value=None)
-					st.markdown(f"##### Evaluation of {'your' if student == st.session_state.active_user else student+"'s"} work on the Final Experiment materials and project"); 
-					final_project = st.number_input(f"How much this person contributed on the Final Experiment Proposal Presentation, inital concept generation, and concept generation revision on a scale from 0 to 5. 0 is no contribution, 5 is full expected individual contribution.", key=f"final_{student}", min_value=0.0, max_value=5.0, step=0.1, format="%.1f", value=None)
-					st.markdown(f"##### Written evaluation of {'your' if student == st.session_state.active_user else student+"'s"} work so far this semester."); 
-					comments = st.text_area(f"Summarize the details of this persons work and contributions to the ME4842 group.", key=f"comments{student}")
+			st.markdown(f'##### Provide any longhand feedback here.')
+			feedback = st.text_area(f"If you provide feedback for individual presenters, identify them by name in your response. Things you might comment on include the following: your evaluation or interpretation of the results, speaker preparedness, elocution, and any recommendations you have for future presentations.", key=f"comments_{group}")
 
-					ind_feedback_dict = {
-						"response_type":'Individual',
-						"active_user": st.session_state.active_user,
-						"student_being_reviewed": student,
-						"labs": labs,
-						"memos_score": memos,
-						"meetings_score": meetings,
-						"final_project_score": final_project,
-						"comments": comments,
-					}
-					ind_scores.append(ind_feedback_dict)
+			ind_feedback_dict = {
+				"active_user": st.session_state.active_user,
+				"completeness": completeness,
+				"technical_content": technical_content,
+				"presentation_quality": presentation_quality,
+				"answering_questions": ability_to_answer_questions,
+				"feedback": feedback,
+			}
+			ind_scores.append(ind_feedback_dict)
 
-				#group survey question
+			#group survey question
 
-				is_complete = all_scores_filled(ind_scores)
-				#on data submission
-				if not is_complete:
-					st.write('Please complete all required fields before submitting.')
-				if st.button("Submit Feedback", disabled = not is_complete):
-					if not st.session_state.awaiting_confirm:
-						if ref.get():
-							st.session_state.awaiting_confirm = True
-						else:
-							for ind_response in ind_scores:
-								ref.push(ind_response)
-							show_review_prompt()
-
-				if st.session_state.awaiting_confirm:
-					st.warning("It looks like you have already submitted feedback for this survey. Would you like to overwrite your previous submission? If not you may close the page.")
-					if st.button('Confirm'):
-						ref.delete()
+			is_complete = all_scores_filled(ind_scores)
+			#on data submission
+			if not is_complete:
+				st.write('Please finish scoring before submitting.')
+			if st.button("Submit Feedback", disabled = not is_complete):
+				if not st.session_state.awaiting_confirm:
 						for ind_response in ind_scores:
 							ref.push(ind_response)
 						show_review_prompt()
-						
+
 	else:
 		st.error(f"Access denied for {sign_in_id or 'unknown user'}.")
 		st.button("Log out", on_click=st.logout)
